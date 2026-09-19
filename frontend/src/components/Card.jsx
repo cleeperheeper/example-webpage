2851ffaaf9424aa92b56fc0b842975bfb6fa87a3
cleeperheeper <imig.seth@yahoo.com>
2026-09-18 20:48:52 -0700

    Experimenting with basic components

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,24 @@
+function Card({info}) {
+
+    function clickFavorite() {
+        alert("clicked!")
+    }
+
+    return <div className="card">
+        <div className="image">
+            <img src={info.img} alt={info.title} />
+            <div className="overlay">
+                <button className="favorite-btn" onClick={clickFavorite}>
+                    ♥
+                </button>
+            </div>
+        </div>
+        <div className="project-info">
+            <h3>{info.title}</h3>
+            <p>{info.year}</p>
+        </div>
+    </div>
+
+}
+
+export default Card
